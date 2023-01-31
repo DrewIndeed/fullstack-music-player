8b381b13d5c04b24c04508dd6d3addb5f263d49b
an.le6 <truongan1730@gmail.com>
2023-01-31 15:31:11 +0700

    1. config: chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,41 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css";
+
+const myTheme = extendTheme({
+  colors: {
+    grays: {
+      100: "#f5f5f5",
+      200: "#eeeeee",
+      300: "#e0e0e0",
+      400: "#bdbdbd",
+      500: "#9e9e9e",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={myTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default App;
